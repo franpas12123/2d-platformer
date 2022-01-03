24c032284c7e66dbfb3d9142780b6140229b29a0
Frances Pasulohan <franpas12123@gmail.com>
2022-01-03 12:58:51 +0800

    add variable jump height

--- src/Actors/Player.gd
+++ src/Actors/Player.gd
@@ -5,15 +5,16 @@ func _ready():
 	print('child process')
 
 func _physics_process(delta):
+	var is_jump_interrupted = Input.is_action_just_released("jump") and velocity.y < 0.0
 	var direction = get_direction()
 
-	velocity = calculate_move_velocity(velocity, direction, speed)
+	velocity = calculate_move_velocity(velocity, direction, speed, is_jump_interrupted)
 	velocity = move_and_slide(velocity, FLOOR_NORMAL)
 
 func get_direction():
 	return Vector2(Input.get_action_strength("move_right") - Input.get_action_strength("move_left"), -1.0 if Input.is_action_just_pressed("jump") and is_on_floor() else 0.0)
 
-func calculate_move_velocity(linear_velocity, direction, speed):
+func calculate_move_velocity(linear_velocity, direction, speed, is_jump_interrupted):
 	var new_velocity = linear_velocity
 	new_velocity.x = direction.x * speed.x
 	new_velocity.y += gravity * get_physics_process_delta_time()
@@ -21,4 +22,6 @@ func calculate_move_velocity(linear_velocity, direction, speed):
 	if direction.y == -1:
 		new_velocity.y = speed.y * direction.y
 	
+	if is_jump_interrupted:
+		new_velocity.y = 0.0
 	return new_velocity
