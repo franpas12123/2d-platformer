a3d7e163a7810cb213a2bcbc4ea79329c44ce586
Frances Pasulohan <franpas12123@gmail.com>
2022-01-03 12:52:09 +0800

    scale down player size

--- src/Actors/Actor.gd
+++ src/Actors/Actor.gd
@@ -2,8 +2,9 @@ extends KinematicBody2D
 
 class_name Actor
 
+const FLOOR_NORMAL = Vector2.UP
+
 export var speed = Vector2(500.0, 800.0)
 export var gravity = 3000.0
 
 var velocity = Vector2.ZERO
-var FLOOR_NORMAL = Vector2.UP
