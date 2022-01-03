6ad741c43e52322225476e5ef441d3d0b9199c78
Frances Pasulohan <franpas12123@gmail.com>
2022-01-03 13:07:06 +0800

    make velocity private

--- src/Actors/Player.gd
+++ src/Actors/Player.gd
@@ -1,27 +1,34 @@
 extends Actor
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print('child process')
+	print("child process")
+
 
 func _physics_process(delta):
-	var is_jump_interrupted = Input.is_action_just_released("jump") and velocity.y < 0.0
+	var is_jump_interrupted = Input.is_action_just_released("jump") and _velocity.y < 0.0
 	var direction = get_direction()
 
-	velocity = calculate_move_velocity(velocity, direction, speed, is_jump_interrupted)
-	velocity = move_and_slide(velocity, FLOOR_NORMAL)
+	_velocity = calculate_move_velocity(_velocity, direction, speed, is_jump_interrupted)
+	_velocity = move_and_slide(_velocity, FLOOR_NORMAL)
+
 
 func get_direction():
-	return Vector2(Input.get_action_strength("move_right") - Input.get_action_strength("move_left"), -1.0 if Input.is_action_just_pressed("jump") and is_on_floor() else 0.0)
+	return Vector2(
+		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
+		-1.0 if Input.is_action_just_pressed("jump") and is_on_floor() else 0.0
+	)
+
 
 func calculate_move_velocity(linear_velocity, direction, speed, is_jump_interrupted):
 	var new_velocity = linear_velocity
 	new_velocity.x = direction.x * speed.x
 	new_velocity.y += gravity * get_physics_process_delta_time()
-	
+
 	if direction.y == -1:
 		new_velocity.y = speed.y * direction.y
-	
+
 	if is_jump_interrupted:
 		new_velocity.y = 0.0
 	return new_velocity
